5eab41853fc6c4a2fa721dc48ae62b496b435184
Kevin Idzik <kdi1996@live.com>
2018-02-22 21:33:41 -0500

    Settings and garbo

--- Assets/Scripts/GyroController.cs
+++ Assets/Scripts/GyroController.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GyroController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class GyroController : MonoBehaviour
+{
+	//Use this for initialization
+	void Start()
+    {
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	//Update is called once per frame
+	void Update()
+    {
 		
 	}
 }
