7a15ec575820960a5b52ed8c13c5163be77522db
Charles Williams <crw4147@rit.edu>
2018-03-01 20:35:37 -0500

    Placed stein tilting in main controls

--- Assets/Scripts/AccelerometerController.cs
+++ Assets/Scripts/AccelerometerController.cs
@@ -7,9 +7,13 @@ public class AccelerometerController : MonoBehaviour
 {
     //DEBUG
     private UnityEngine.UI.Text[] tiltAmounts = new UnityEngine.UI.Text[2]; //UI text
-    //private UnityEngine.UI.Image[] steins = new UnityEngine.UI.Image[1];
+
+    // lists for holding beer and steins
     private List<UnityEngine.UI.Image> steins;
     private List<UnityEngine.UI.Image> beers;
+
+    // rate at which stein turns
+    public float steinRotateSpeed = 1f;
 
     //Use this for initialization
     void Start()
@@ -35,8 +39,8 @@ public class AccelerometerController : MonoBehaviour
     {
 #if UNITY_EDITOR //Debug controls
         transform.Translate(Input.GetAxis("Horizontal") * .1f, 0, Input.GetAxis("Vertical") * .1f); //Move the object that this script is attached to
-        steins[0].transform.Rotate(Vector3.forward, 1 * -Input.GetAxis("Horizontal"));
-        beers[0].transform.Rotate(Vector3.forward, 1 * Input.GetAxis("Horizontal"));
+        steins[0].transform.Rotate(Vector3.forward, steinRotateSpeed * -Input.GetAxis("Horizontal"));
+        beers[0].transform.Rotate(Vector3.forward, steinRotateSpeed * Input.GetAxis("Horizontal"));
 #else
         Vector3 accelerometer = Input.acceleration * .1f; //Get the acceleration, dull it down a bit
 
@@ -48,6 +52,11 @@ public class AccelerometerController : MonoBehaviour
 
         transform.Translate(accelerometer.x, 0, -accelerometer.z); //Move the object that this script is attached to
 
+        if (Mathf.Abs(accelerometer.x) < .05f)
+            accelerometer.x = 0;
+
+        steins[0].transform.Rotate(Vector3.forward, steinRotateSpeed * -accelerometer.x);
+        beers[0].transform.Rotate(Vector3.forward, steinRotateSpeed * accelerometer.x;
         //DEBUG
         tiltAmounts[0].text = "X: " + accelerometer.x;
         tiltAmounts[1].text = "Z: " + accelerometer.z;
