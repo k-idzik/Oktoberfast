a6f451b47b5192b9147503b198e338d026744f6f
Kevin Idzik <kdi4293@ad.rit.edu>
2018-03-01 20:50:08 -0500

    Fixed name and beer rotation

--- Assets/Scripts/AccelerometerController.cs
+++ Assets/Scripts/AccelerometerController.cs
@@ -52,11 +52,12 @@ public class AccelerometerController : MonoBehaviour
 
         transform.Translate(accelerometer.x, 0, -accelerometer.z); //Move the object that this script is attached to
 
-        if (Mathf.Abs(accelerometer.x) < .05f)
+        if (Mathf.Abs(accelerometer.x) < .03f)
             accelerometer.x = 0;
 
         steins[0].transform.Rotate(Vector3.forward, steinRotateSpeed * -accelerometer.x);
         beers[0].transform.Rotate(Vector3.forward, steinRotateSpeed * accelerometer.x;
+
         //DEBUG
         tiltAmounts[0].text = "X: " + accelerometer.x;
         tiltAmounts[1].text = "Z: " + accelerometer.z;
