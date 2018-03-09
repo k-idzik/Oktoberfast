b8756b9158b43530439835dc033c5a6be61adde4
Josh Malmquist <joshmalmquist96@gmail.com>
2018-03-09 02:08:43 -0500

    Created Endzone Area
    When the player enters the endzone area, it will trigger the end level screen.
    
    Player prefab tag changed to "Player"

---
+++ Assets/Scripts/EndLevel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevel : MonoBehaviour {
+
+    MenuManager menuManager;
+
+	// Use this for initialization
+	void Start () {
+        menuManager = GameObject.Find("MenuManager").GetComponent<MenuManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //Inputing default values for now
+            menuManager.ShowEndLevelScreen(0, 0, 1.00, 0);
+        }
+
+    }
+}
