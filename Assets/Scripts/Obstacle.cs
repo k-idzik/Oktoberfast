6d29f2e8cc1cbee3e3b0ecd6755639c86580c685
Kevin Idzik <kdi1996@live.com>
2018-03-30 12:03:01 -0400

    Feedback implementation
    -Tables blink when collided with
    -Updated icon

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -2,31 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Obstacle : MonoBehaviour {
+public class Obstacle : MonoBehaviour
+{
     private MeshRenderer renderer;
+    private Collider thisCollider;
+    private int rendererTimer = 0; //Timer for mesh blink
 
-	// Use this for initialization
-	void Start () {
+	//Use this for initialization
+	void Start()
+    {
         renderer = GetComponent<MeshRenderer>();
+        thisCollider = GetComponent<Collider>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	//Update is called once per frame
+	void Update()
+    {
+
 	}
 
     //Colliding with table
-    private void OnTriggerEnter(Collider coll)
+    private void OnCollisionEnter(Collision coll)
     {
+        if (coll.gameObject.tag == "Player")
+        {
+            thisCollider.isTrigger = true;
+        }
+    }
 
+    private void OnTriggerStay(Collider coll)
+    {
+        if (rendererTimer < 10)
+            renderer.enabled = false;
+        else
+            renderer.enabled = true;
+
+        rendererTimer++;
+
+        if (rendererTimer > 20)
+            rendererTimer = 0;
     }
 
     //Leaving collision with table
     private void OnTriggerExit(Collider coll)
     {
-       
+        if (coll.gameObject.tag == "Player")
+        {
+            thisCollider.isTrigger = false; //Make this a collider again
+            rendererTimer = 0; //Reset the timer
+            renderer.enabled = true; //Turn the renderer back on
+        }
     }
-
-
-    
 }
